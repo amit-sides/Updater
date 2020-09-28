88c1a76895c1b5d51a6d0406674d665abc5af327
Amit Sides <amit794@walla.com>
2020-09-28 21:23:05 +0300

    fixed default settings option

--- Updater/settings.py
+++ Updater/settings.py
@@ -38,42 +38,31 @@ __values__.update(dict(
 def init_settings(save=True):
     global __values__
 
-    __values__.update(dict(
-        # Dynamic settings (created automatically by the settings above)
-
-        REGISTRY_PATH=rf"Software\{__values__['SOFTWARE_NAME']}\{__values__['UPDATER_NAME']}",
-        SOFTWARE_PATH=rf"{PROGRAM_FILES}{os.path.sep}{__values__['SOFTWARE_NAME']}",
-    ))
-
-    __values__.update(dict(
-        # Dynamic settings (created automatically by the settings above)
-
-        UPDATER_PATH=rf"{__values__['SOFTWARE_PATH']}{os.path.sep}{__values__['UPDATER_NAME']}",
-        LAUNCHER_PATH=rf"{__values__['SOFTWARE_PATH']}{os.path.sep}{__values__['LAUNCHER_NAME']}",
-    ))
-
-    __values__.update(dict(
-        # Dynamic settings (created automatically by the settings above)
-
-        SETTINGS_PATH=rf"{__values__['UPDATER_PATH']}{os.path.sep}settings.json",
-        LOGGER_PATH=rf"{__values__['UPDATER_PATH']}{os.path.sep}log.txt",
-        UPDATE_PATH=rf"{__values__['UPDATER_PATH']}{os.path.sep}update.zip",
-
-        AUTO_INSTALLATIONS_REGISTRY=rf"{__values__['REGISTRY_PATH']}\auto_update",
-        UPDATING_SERVER_REGISTRY=rf"{__values__['REGISTRY_PATH']}\update_server",
-        PORT_REGISTRY=rf"{__values__['REGISTRY_PATH']}\port",
-        RSA_MODULO_REGISTRY=rf"{__values__['REGISTRY_PATH']}\rsa_modulo",
-        RSA_PUBLIC_REGISTRY=rf"{__values__['REGISTRY_PATH']}\rsa_public",
-        RSA_PRIVATE_REGISTRY=rf"{__values__['REGISTRY_PATH']}\rsa_private",
-        UPDATE_MAJOR_REGISTRY=rf"{__values__['REGISTRY_PATH']}\update_major",
-        UPDATE_MINOR_REGISTRY=rf"{__values__['REGISTRY_PATH']}\update_minor",
-        VERSION_MAJOR_REGISTRY=rf"{__values__['REGISTRY_PATH']}\version_major",
-        VERSION_MINOR_REGISTRY=rf"{__values__['REGISTRY_PATH']}\version_minor",
-        ADDRESS_ID_REGISTRY=rf"{__values__['REGISTRY_PATH']}\address_id",
-        UPDATE_REGISTRY_FORMAT=rf"{__values__['REGISTRY_PATH']}\Update_{{}}",
-    ))
-
+    # Load existing settings
     load_settings()
+
+    # Setting default settings values, if they don't appear in the settings.json file already
+    __values__.setdefault("REGISTRY_PATH", rf"Software\{__values__['SOFTWARE_NAME']}\{__values__['UPDATER_NAME']}")
+    __values__.setdefault("SOFTWARE_PATH", rf"{PROGRAM_FILES}{os.path.sep}{__values__['SOFTWARE_NAME']}")
+    __values__.setdefault("UPDATER_PATH", rf"{__values__['SOFTWARE_PATH']}{os.path.sep}{__values__['UPDATER_NAME']}")
+    __values__.setdefault("LAUNCHER_PATH", rf"{__values__['SOFTWARE_PATH']}{os.path.sep}{__values__['LAUNCHER_NAME']}")
+    __values__.setdefault("SETTINGS_PATH", rf"{__values__['UPDATER_PATH']}{os.path.sep}settings.json")
+    __values__.setdefault("LOGGER_PATH", rf"{__values__['UPDATER_PATH']}{os.path.sep}log.txt")
+    __values__.setdefault("UPDATE_PATH", rf"{__values__['UPDATER_PATH']}{os.path.sep}update.zip")
+    __values__.setdefault("AUTO_INSTALLATIONS_REGISTRY", rf"{__values__['REGISTRY_PATH']}\auto_update")
+    __values__.setdefault("UPDATING_SERVER_REGISTRY", rf"{__values__['REGISTRY_PATH']}\update_server")
+    __values__.setdefault("PORT_REGISTRY", rf"{__values__['REGISTRY_PATH']}\port")
+    __values__.setdefault("RSA_MODULO_REGISTRY", rf"{__values__['REGISTRY_PATH']}\rsa_modulo")
+    __values__.setdefault("RSA_PUBLIC_REGISTRY", rf"{__values__['REGISTRY_PATH']}\rsa_public")
+    __values__.setdefault("RSA_PRIVATE_REGISTRY", rf"{__values__['REGISTRY_PATH']}\rsa_private")
+    __values__.setdefault("UPDATE_MAJOR_REGISTRY", rf"{__values__['REGISTRY_PATH']}\update_major")
+    __values__.setdefault("UPDATE_MINOR_REGISTRY", rf"{__values__['REGISTRY_PATH']}\update_minor")
+    __values__.setdefault("VERSION_MAJOR_REGISTRY", rf"{__values__['REGISTRY_PATH']}\version_major")
+    __values__.setdefault("VERSION_MINOR_REGISTRY", rf"{__values__['REGISTRY_PATH']}\version_minor")
+    __values__.setdefault("ADDRESS_ID_REGISTRY", rf"{__values__['REGISTRY_PATH']}\address_id")
+    __values__.setdefault("UPDATE_REGISTRY_FORMAT", rf"{__values__['REGISTRY_PATH']}\Update_{{}}")
+
+    # Saves the settings, if they should be saved
     if save:
         save_settings()
 
